5cba3fd7b085e8e73ffc135d61ee8d99a6846c01
Anicoder2004 <aniklancer2004@gmail.com>
2026-04-04 19:25:01 +0600

    GCD for Three Numbers

---
+++ GCDThreNum.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long get_validated_int(const char* label) {
+    char buffer[100];
+    char *endptr;
+
+    printf("Enter %s: ", label);
+    
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    long long value = strtoll(buffer, &endptr, 10);
+
+    // 1. Must be a number (endptr != buffer)
+    // 2. Must not have decimals (endptr must point to newline)
+    if (endptr == buffer || (*endptr != '\n' && *endptr != '\0')) {
+        printf("\nError: Invalid input. Please enter a whole number only.\n");
+        exit(EXIT_FAILURE); }
+
+    else if (value<0) {
+        printf("\nError: You should enter positive numbers only.\n");
+        exit(EXIT_FAILURE); }
+
+    return value;
+}
+
+// Euclidean Algorithm
+long long calculate_gcd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    long long num1, num2, num3, gcd;
+
+    printf("--- Greatest Common Divisor (GCD) ---\n");
+
+    num1 = get_validated_int("first number");
+    num2 = get_validated_int("second number");
+    num3 = get_validated_int("third number");
+
+    if (num1 == 0 && num2 == 0 && num3 == 0) {
+        printf("\nGCD of three 0's is undefined.\n");
+    } else {
+        gcd = calculate_gcd(calculate_gcd(num1, num2),num3);
+        
+        printf("\nThe GCD of %lld, %lld and %lld is: %lld\n", num1, num2, num3, gcd);
+    }
+
+    return 0;
+}
